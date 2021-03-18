200099c0091465c8a6c62de637980b84c84e9b51
Dmitriy Vorozheykin <lmidorunl@gmail.com>
2021-03-19 01:18:34 +0400

    feat: add getFormDataObj service

---
+++ lab_3/src/services/getFormDataObj.js
@@ -0,0 +1,19 @@
+import nextId from './nextId';
+
+const getFormDataObj = (form) => {
+    let formDataObj = {};
+
+    const formFields = [...form.elements].map(input => input.id);
+    const formData = [...form.elements].map(input => input.value);
+
+    form.reset();
+
+    formFields.forEach((field, i) => {
+        formDataObj[field] = formData[i];
+    })
+    formDataObj['id'] = nextId();
+
+    return formDataObj;
+}
+
+export default getFormDataObj;
